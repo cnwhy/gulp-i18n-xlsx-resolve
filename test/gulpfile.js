4a159707bdac8f8c6258df5883e13acfc8f0e9f5
cnwhy <w.why@163.com>
2017-09-22 16:12:06 +0800

    v0.1.0

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -11,12 +11,23 @@ gulp.task('i18n',function(){
         ,'english':"en"
     };
 
-    //生成json
+    //生成json 各语言独立语言包
     files.pipe(i18n({
         'type':'json'   // 输出为 'ini' or 'json' 默认 'json'
         ,'keyColumnName':"key"  //引用列的名称 默认为 'key'
         ,'passColumns':['bak']  //不输出的列
         ,'langMap':langMap      //语言映射
+    }))
+    .pipe(gulp.dest('./language'))
+
+    //生成json 生成一个翻译文件
+    files.pipe(i18n({
+        'type':'json'   // 输出为 'ini' or 'json' 默认 'json'
+        ,'keyColumnName':"key"  //引用列的名称 默认为 'key'
+        ,'passColumns':['bak']  //不输出的列
+        ,'langMap':langMap      //语言映射
+        ,'concat': 'langs'      //合并成一个文件
+        ,'nosplit': true        //不解析KEY
     }))
     .pipe(gulp.dest('./language'))
 
